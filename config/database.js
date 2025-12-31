a0b2c62bc232bfcde46c191c267ef80d9ce79854
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2025-12-31 15:19:08 +0530

    updated code

--- config/database.js
+++ config/database.js
@@ -5,10 +5,12 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI,{
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI
+    //   ,{
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // }
+  );
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
